cff2b320d0c35589fa597ddc6146e09a3a435149
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2017-08-22 09:12:31 -0700

    Create Single View Project

---
+++ iBeacon/iBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacon
+//
+//  Created by Nishant Mendiratta on 8/22/17.
+//  Copyright © 2017 Nishant Mendiratta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
